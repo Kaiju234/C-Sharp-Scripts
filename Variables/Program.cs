e0df4e9643a5be5430c631850bec6e067706df58
Raj Shah <68970395+Kaiju234@users.noreply.github.com>
2023-12-22 10:00:57 -0600

    Add files via upload

---
+++ Variables/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Variables
+{
+    class Variables
+    {
+        static void Main(string[] args)
+        {
+            int x; // declaration
+            x = 123; // intialization
+
+
+            int y = 321; // Declaration + Intilization 
+
+            int z = x + y;
+
+            int age = 19; //whole integer
+            double height = 5.11; //decimal number
+            bool alive = false; // True or False
+            char symbol = '@';
+            String name = "Raj";
+            Console.WriteLine("Hello" + name);
+            Console.WriteLine("Your age is " + age);
+            Console.WriteLine("Your height is " + height + "cm");
+            Console.WriteLine("Are you Alive" + alive);
+            Console.WriteLine("Your symbol is: " + symbol);
+
+            String userName = symbol + name;
+
+            Console.WriteLine("your username is: " + userName);
+
+            Console.ReadKey();
+        }
+    }
+} 
